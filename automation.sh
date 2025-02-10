e800a1886d1c9d217f27290e2c3218eeb30c0e49
mattkah <mkahler72@gmail.com>
2025-02-10 10:47:19 -0800

    updating the script to include new SNPs field

--- automation.sh
+++ automation.sh
@@ -8,9 +8,9 @@ awk '$3 == "gene" {print $1 "\t" $3 "\t" $4 "\t" $5 "\t" $9}' Vigna_unguiculata.
 
 # Removing the header from cowpea_test.tsv and Compara
 
-cut -f22,23,24 snps_cowpea.tsv | sed -e 's/Vu0//g ; s/Vu//g' | tail -n +2 > new.tsv
+cut -f22,23,24,26 snps_cowpea.tsv | sed -e 's/Vu0//g ; s/Vu//g' | tail -n +2 > new.tsv
 
-tail -n +2 Compara_cowpea_homologs_ara.tsv > new2.tsv
+cut -f1,3,4,5,6,7,8 Compara_cowpea_homologs_ara.tsv | tail -n +2 > new2.tsv
 
 # Input files
 
@@ -26,11 +26,12 @@ while IFS=$'\t' read -r line; do
     chr1=$(echo "$line" | awk '{print $1}')
     start1=$(echo "$line" | awk '{print $2}')
     end1=$(echo "$line" | awk '{print $3}')
+    snp1=$(echo "$line" | awk '{print $4}')
  
    chromosome_dir="${output_dir}/chr${chr1}"
    mkdir -p "$chromosome_dir"
 
-   output_file="${chromosome_dir}/${chr1}_${start1}_${end1}.tsv"
+   output_file="${chromosome_dir}/${chr1}_${start1}_${end1}.${snp1}.tsv"
 
    matched_genes=$(awk -v chr="$chr1" -v start="$start1" -v end="$end1" \
    '$1 == chr && $3 >= start && $4 <= end {print $5}' "$data2")
@@ -39,7 +40,7 @@ while IFS=$'\t' read -r line; do
     for gene_id_mod in $matched_genes; do
       core=$(echo "$gene_id_mod" | awk -F '\t' '{if ($1 ~/ID=gene:/){match($1, /Vigun[^;]+/); print substr($1, RSTART, RLENGTH)}}')
 
-    matching_species=$(grep -w "$core" "$data3")
+    matching_species=$(grep -w "$core" "$data3" | sort -u)
 
     if [[ -n "$matching_species" ]]; then
       echo "$matching_species" >> "$output_file"
