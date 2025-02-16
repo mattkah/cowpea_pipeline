e5155c15ff2e43ab3a0a69bc8e2a9e99164b9ffb
mattkah <mkahler72@gmail.com>
2025-02-16 03:03:04 -0800

    modifying search.sh script to effectively look for snps by search

--- search.sh
+++ search.sh
@@ -1,18 +1,17 @@
 #!/bin/zsh
 
-target="/Users/mattkahler/Desktop/cowpea_test/output_data/"
-
 while true; do
   echo -e "Enter SNP: \c"
   read snp
 
-cd /Users/mattkahler/Desktop/cowpea_test
-if grep -wq "$snp" new.tsv; then
-  cd output_data/ 
-  file="$(find . -type f -iname "*.$snp.tsv")"
-  open "$file"
-else
-  echo "Please try again."
+ cd /Users/mattkahler/Desktop/cowpea_test
+ 
+ if grep -wq "$snp" new.tsv; then
+   cd output_data/ 
+   file="$(find . -type f -iname "*.$snp.tsv")"
+   open "$file"
+ else
+   echo "Please try again."
 fi
 done
 
